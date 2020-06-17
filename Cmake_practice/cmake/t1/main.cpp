a1d213ebad0de72bd996f56af28e6bad3e931533
MagicTZ <tz.paul1996@gmail.com>
2020-06-17 09:43:00 +0200

    HoloLens房间重建论文以及CMake练习

---
+++ Cmake_practice/cmake/t1/main.cpp
@@ -0,0 +1,8 @@
+//main.c
+#include<iostream>
+
+int main()
+{
+	std::cout<<"Hello World from t1 Main!\n";
+	return 0;
+}
---
+++ Cmake_practice/cmake/t5/build/src/CMakeFiles/curltest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/curltest.dir/main.cpp.o"
+  "curltest"
+  "curltest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/curltest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
